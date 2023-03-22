59e55f9b444abec18c06e3ae440c89f4d28ba0e5
thanhlh <Hlhnaht04)*@@>
2023-03-22 18:02:05 +0700

    update code Migration

--- src/main/resources/migration/V03__Insert_CATEGORY_Data.sql
+++ src/main/resources/migration/V03__Insert_CATEGORY_Data.sql
@@ -1,6 +1,6 @@
 INSERT INTO CATEGORY(ID, IMAGE, NAME)
-VALUES (1, "1", "Loai 1"),
-       (2, "1", "Loai 2"),
-       (3, "1", "Loai 3"),
-       (4, "1", "Loai 4"),
-       (5, "1", "Loai 5");
+VALUES (1, "bacadi-1.jpg", "Loai 1"),
+       (2, "chivas-18-blue-hq-2021-3.jpg", "Loai 2"),
+       (3, "ruou-vodka-hanoi-700-ml.jpg", "Loai 3"),
+       (4, "malibu_dua.jpg", "Loai 4"),
+       (5, "ruou-chivas21-nam-510x480.jpg", "Loai 5");
--- src/main/resources/migration/V04__Insert_TRADE_MARK_Data.sql
+++ src/main/resources/migration/V04__Insert_TRADE_MARK_Data.sql
@@ -1,6 +1,6 @@
 INSERT INTO TRADE_MARK(ID, IMAGE, NAME)
-VALUES (1, "1", "San pham 1"),
-       (2, "1", "San pham 2"),
-       (3, "1", "San pham 3"),
-       (4, "1", "San pham 4"),
-       (5, "1", "San pham 5");
+VALUES (1, "bacadi-1.jpg", "San pham 1"),
+       (2, "chivas-18-blue-hq-2021-3.jpg", "San pham 2"),
+       (3, "ruou-vodka-hanoi-700-ml.jpg", "San pham 3"),
+       (4, "malibu_dua.jpg", "San pham 4"),
+       (5, "ruou-chivas21-nam-510x480.jpg", "San pham 5");
